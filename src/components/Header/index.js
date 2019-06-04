f03120685f436a5c76c98ca3dc01dc2e7a136b3d
Daniel Barben <daniel.barben@tamedia.ch>
2019-06-04 21:26:37 +0200

    new Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Header.css';
+import Logoheader from '../../img/logoheader.png'
 
 function Header() {
-  return (<div className="Header"><h1>BernerWasser</h1><p>ein Quiz der BZ Berner Zeitung</p></div>)
+  return (<div className='Header'><img src={Logoheader} className='Logoheader' key='12' alt='digitalOrakel'/></div>)
+  //return (<div className="Header"><h1>BernerWasser</h1><p>ein Quiz der BZ Berner Zeitung</p></div>)
 }
 
 export default Header;
