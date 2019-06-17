cd6bc4c3a79d33c865cb41c1324f6586c8e58d3c
Daniel Barben <daniel.barben@tamedia.ch>
2019-06-17 18:06:12 +0200

    new text

--- src/components/Landingpage/index.js
+++ src/components/Landingpage/index.js
@@ -13,8 +13,8 @@ class Landingpage extends Component {
       <div className="Landingpage bgimg gamebox">
         <h2 className="intro title">BernerWasser</h2>
         <p className="subtitle">Das Quiz zur Aareserie</p>
-        <p className="text">Wie gut kennen Sie sich mit den Berner Seen und Flüssen aus? Testen Sie Ihr Wissen.</p>
-        <p className="text">Quiz starten:</p>
+        <p className="text">Haben Sie in der Schule in Geografie eher geschlafen oder wissen Sie richtig gut Bescheid über die Berner Flüsse und Seen?</p>
+        <p className="text">Testen Sie Ihr Wissen in unserem Quiz.</p>
         <span className="button intro" onClick = {() => this.props.gameStart('leicht')}>leicht</span><span className="button intro" onClick = {() => this.props.gameStart('mittel')}>mittel</span><span className="button intro" onClick = {() => this.props.gameStart('schwierig')}>schwer</span>
         </div>
     );
